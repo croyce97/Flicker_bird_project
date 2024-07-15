4d0ec577327a8ab5c45c211be96c63d81e285dce
Nguyễn Quang Cảnh <125856531+croyce97@users.noreply.github.com>
2024-07-15 19:54:55 +0700

    Add files via upload

---
+++ defs.h
@@ -0,0 +1,18 @@
+#ifndef DEFS_H_
+#define DEFS_H_
+
+#define WIDTH 479
+#define HEIGHT 639
+
+#define FPS 60
+#define frameDelay 1000 / FPS
+
+#define GRAVITY 80.0f
+
+#define BIRD_WIDTH 55
+#define BIRD_HEIGHT 39
+
+#define GROUND_HEIGHT 112
+#define GROUND_WIDTH 336
+
+#endif
